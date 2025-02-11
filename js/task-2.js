b48e68fde99e5d9996d338331a2e0a13198673cc
Yuriysps <166153092+Yuriysps@users.noreply.github.com>
2025-02-11 21:39:36 +0200

    Add files via upload

---
+++ js/task-2.js
@@ -0,0 +1,70 @@
+console.log('task-2.js');
+//! document.getElementById("task-3/1").innerHTML = 
+
+
+
+
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+const getUsersWithFriend =(allUsers,friendName) => allUsers.filter(user => user.friends.includes(friendName));
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+ document.getElementById("task-2/1").innerHTML = getUsersWithFriend(allUsers, "Briana Decker");
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+ document.getElementById("task-2/2").innerHTML = getUsersWithFriend(allUsers, "Goldie Gentry");
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+ document.getElementById("task-2/3").innerHTML = getUsersWithFriend(allUsers, "Adrian Cross");
+
+
